95ee5de41e72c98eea9b7f90b42fb894c8e9fec7
Victor Sousa <victor.sousa@epitech.eu>
2020-04-26 20:29:44 +0200

    [-] tslint

--- src/app/controller/rest.controller.ts
+++ src/app/controller/rest.controller.ts
@@ -48,8 +48,7 @@ export class RestController<T extends Document> {
     @safethrow
     public async delete(req: Request, res: Response) {
         const item = await this.collection.deleteOne({_id: req.params.id} as FilterQuery<T>);
-        console.log(item)
-        if (!item || !item.deletedCount ) {
+        if (!item || !item.deletedCount) {
             ApiError.ItemNotFound()
         }
         res.status(204).end();
